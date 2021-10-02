75128cd24cef19ad42a3209f7b6251b5fa73977f
Casual <hyypercasual@gmail.com>
2021-10-02 13:02:43 +0800

    Добавьте файлы проекта.

---
+++ LAB_2/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace LAB_2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textA = new System.Windows.Forms.TextBox();
+            this.textB = new System.Windows.Forms.TextBox();
+            this.MainButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TaskButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textA
+            // 
+            this.textA.Location = new System.Drawing.Point(13, 13);
+            this.textA.Name = "textA";
+            this.textA.Size = new System.Drawing.Size(100, 23);
+            this.textA.TabIndex = 0;
+            // 
+            // textB
+            // 
+            this.textB.Location = new System.Drawing.Point(12, 56);
+            this.textB.Name = "textB";
+            this.textB.Size = new System.Drawing.Size(100, 23);
+            this.textB.TabIndex = 1;
+            // 
+            // MainButton
+            // 
+            this.MainButton.Location = new System.Drawing.Point(12, 105);
+            this.MainButton.Name = "MainButton";
+            this.MainButton.Size = new System.Drawing.Size(296, 44);
+            this.MainButton.TabIndex = 2;
+            this.MainButton.Text = "Режем на квадраты";
+            this.MainButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(120, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Первая сторона прямоугольника";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(120, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Вторая сторона прямоугольника";
+            // 
+            // TaskButton
+            // 
+            this.TaskButton.Location = new System.Drawing.Point(353, 16);
+            this.TaskButton.Name = "TaskButton";
+            this.TaskButton.Size = new System.Drawing.Size(99, 133);
+            this.TaskButton.TabIndex = 5;
+            this.TaskButton.Text = "Задание";
+            this.TaskButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 161);
+            this.Controls.Add(this.TaskButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MainButton);
+            this.Controls.Add(this.textB);
+            this.Controls.Add(this.textA);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(480, 200);
+            this.MinimumSize = new System.Drawing.Size(480, 200);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textA;
+        private System.Windows.Forms.TextBox textB;
+        private System.Windows.Forms.Button MainButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button TaskButton;
+    }
+}
+
